071e3ec9bca3416d7e8b737420152a1e921f5b2a
Bruno Camargo <bruno.camargo_@outlook.com>
2017-10-22 19:03:08 -0200

    correction on total pages calculations

--- src/main/webapp/GitRepositories/js/controller.js
+++ src/main/webapp/GitRepositories/js/controller.js
@@ -6,11 +6,11 @@ app.controller("topInformationCtrl", function($scope, $http) {
 	$scope.getRepositories = function(){
 		$http({
 			method: 'GET',
-			url: '/services/gitRepositories/getStarredRepositories/' + $scope.page
+			url: 'http://localhost:8080/topInformation/services/gitRepositories/getStarredRepositories/' + $scope.page
 		}).then(function successCallback(response) {
 			var json = angular.fromJson(response.data);
 			$scope.rows = json.items;
-			$scope.totalPages = Math.floor((json.totalItems / 30) + 1);
+			$scope.totalPages = Math.ceil(json.totalItems / 30);
 		});
 	}
 	$scope.getRepositories();
